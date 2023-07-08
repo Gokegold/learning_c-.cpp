66ed41829fe3579a6a2dc395825c7fe371153459
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-07-08 14:41:43 +0100

    Create learning.cpp

---
+++ learning.cpp
@@ -0,0 +1,32 @@
+"""
+Learing
+
+Learning Source: https://www.youtube.com/watch?v=ZzaPdXTrSb8&ab_channel=ProgrammingwithMosh
+
+Twitter @i_amgoke: https://twitter.com/i_amgoke
+
+Github: https://www.github.com/Gokegold
+
+Function: C++ lessonings
+
+Location: Nigeria
+
+Date Created: JULY 7, 2023
+
+last modification:: [JULY 7, 2023]...
+
+"""
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 1;
+  int b = 2;
+  int c = a;
+  a = b;
+  b = c;
+  cout << a;
+  cout << b;
+}
